8435350bad1813d680ce2407f1553a5dbb7906d4
Jedy24 <reihimawari27@gmail.com>
2024-03-13 12:19:48 +0700

    Routing

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -3,6 +3,8 @@
 use Illuminate\Routing\Router;
 use App\Admin\Controllers\UserBrainysController;
 use App\Admin\Controllers\UserMaterialController;
+use App\Admin\Controllers\UserSyllabusController;
+use App\Admin\Controllers\UserExerciseController;
 
 Admin::routes();
 
@@ -16,7 +18,12 @@ Route::group([
     $router->get('/', 'HomeController@index')->name('home');
     $router->get('user-brainys/seed', [UserBrainysController::class, 'seed'])->name('user-brainys.seed');
     $router->get('user-materials/seed', [UserMaterialController::class, 'seed'])->name('user-materials.seed');
+    $router->get('user-syllabi/seed', [UserSyllabusController::class, 'seed'])->name('user-syllabi.seed');
+    $router->get('user-exercises/seed', [UserExerciseController::class, 'seed'])->name('user-exercises.seed');
+
     $router->resource('user-brainys', UserBrainysController::class);
     $router->resource('user-materials', UserMaterialController::class);
+    $router->resource('user-syllabi', UserSyllabusController::class);
+    $router->resource('user-exercises', UserExerciseController::class);
 });
 
